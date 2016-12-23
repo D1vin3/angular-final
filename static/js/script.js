9656e43d684627c323bb85710fb8436317b029c8
d1vin3 <temirlan.nurmakhanov@is.sdu.edu.kz>
2016-12-24 05:43:44 +0600

    search by values

--- static/js/script.js
+++ static/js/script.js
@@ -3,9 +3,12 @@ var app = angular.module('app', ['ngRoute', 'ngStorage']);
 
 // app.run(function($localStorage, $sessionStorage) {});
 
-app.controller('appCtrl', function($scope, $localStorage){
+app.controller('appCtrl', function($scope, $localStorage, $location){
   console.log('appcTRl is working');
 
+  $scope.filterChange = function(){
+    $location.path("search/");
+  }
 })
 
 
@@ -74,15 +77,122 @@ app.factory('myFactory', function($localStorage){
 
 
 app.controller('postsCtrl', function($scope, postsFactory){
-  console.log("postsFactory", postsFactory);
   $scope.posts = postsFactory;
 })
 
 app.controller('postCtrl', function($scope, $routeParams, postsFactory){
-  // console.log($routeParams.postId);
   var postId = Number($routeParams.postId);
   $scope.post = _.findWhere(postsFactory, {id: postId});
 })
+
+app.controller('homeCtrl', function($scope, categoryFactory){
+  console.log('homeCtrl is working');
+  $scope.categories = categoryFactory.slice(0,3);
+})
+
+app.controller('homeCategoryCtrl', function($scope, $routeParams, categoryFactory){
+  var categoryType = $routeParams.categoryType;
+  $scope.goods = _.filter(categoryFactory, {category: categoryType});
+})
+
+
+app.controller('searchCtrl', function($scope, categoryFactory){
+
+  // console.log('searchCtrl is working');
+  $scope.options = categoryFactory;
+
+  $scope.result = $scope.options.filter(function(el){
+    return (el.category.toLowerCase().includes($scope.filter_value) ||
+            el.goodName.toLowerCase().includes($scope.filter_value) ||
+            el.goodDescr.toLowerCase().includes($scope.filter_value)||
+            el.img.toLowerCase().includes($scope.filter_value)
+      );
+  })
+  console.log($scope.result);
+
+  $scope.selectRes = function(result){
+    $scope.res_category = result['category'];
+    $scope.res_description = result['goodDescr'];
+    $scope.res_price = result['price'];
+    $scope.res_img = result['img'];
+  }
+
+})
+
+
+
+app.factory('categoryFactory', function(){
+  return [
+    {
+      category: 'Women',
+      imgSrc: 'women.jpg',
+      goodName: 'Coat',
+      goodDescr: 'Nice coat',
+      img: 'coat.jpg',
+      price: 500,
+    },
+    {
+      category: 'Men',
+      imgSrc: 'man.jpg',
+      goodName: 'Shift',
+      goodDescr: 'Dark shirt',
+      img: 'shirt.jpg',
+      price: 600,
+    },
+    {
+      category: 'Kids',
+      imgSrc: 'kids.jpg',
+      goodName: 'Shorts',
+      goodDescr: 'Set',
+      img: 'kid1.jpg',
+      price: 900,
+    },
+    {
+      category: 'Women',
+      goodName: 'Bag',
+      goodDescr: 'Summer dress',
+      img: 'dress2.jpg',
+      price: 500,
+    },
+    {
+      category: 'Men',
+      goodName: 'Jacket',
+      goodDescr: 'Light jacket',
+      img: 'jacket2.jpg',
+      price: 500,
+    },
+    {
+      category: 'Kids',
+      goodName: 'Candies',
+      goodDescr: 'Really big candies',
+      img: 'kid2.jpg',
+      price: 500,
+    },
+    {
+      category: 'Women',
+      goodName: 'Bag',
+      goodDescr: 'Nice dress',
+      img: 'dress.jpg',
+      price: 600,
+    },
+    {
+      category: 'Men',
+      goodName: 'Jacket',
+      goodDescr: 'Warm jacket',
+      img: 'jacket.jpg',
+      price: 500,
+    },
+    {
+      category: 'Kids',
+      goodName: 'Jeans',
+      goodDescr: '4-5y jeans',
+      img: 'kid3.jpeg',
+      price: 300,
+    },
+
+  ]
+})
+
 
 app.factory('postsFactory', function(){
   return [
@@ -105,7 +215,12 @@ app.factory('postsFactory', function(){
 app.config(function($routeProvider){ 
   $routeProvider
     .when('/', {
-      templateUrl: 'templates/home.html'
+      templateUrl: 'templates/home.html',
+      controller: 'homeCtrl'
+    })
+    .when('/goods/:categoryType', {
+      templateUrl: 'templates/goods.html',
+      controller: 'homeCategoryCtrl'
     })
     .when('/posts', {
       templateUrl: 'templates/posts.html',
@@ -123,6 +238,10 @@ app.config(function($routeProvider){
       templateUrl: 'templates/signup.html',
       controller: 'regCtrl'
     })
+    .when('/search', {
+      templateUrl: 'templates/search.html',
+      controller: 'searchCtrl'
+    })
     .when('/shop',{
       templateUrl: 'templates/shop.html'
     })
@@ -137,20 +256,3 @@ app.config(function($routeProvider){
 
 
 
-
-//   $scope.getContent = function() {
-//   var url = "http://localhost/Midterm2/content.php";
-//   $http.get(url + "?mail=" +$scope.mail + "&password=" + $scope.password ).then(function(response) {
-//     if(response.data == "ok"){
-//       console.log("hello");
-//         $scope.popup2 = {
-//           "display": "block"
-//         }
-//         $scope.popup = {
-//           "display": "none"
-//         }
-//     }
-//     });
-
-//   }
-// })
